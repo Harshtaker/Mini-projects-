540172ab8e6241ed1e443e186fdc02c7c183dce6
Harsh Shukla <harshshukla0303@gmail.com>
2025-07-31 10:25:27 +0530

    Add files via upload

---
+++ Task 1 .cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    srand(time(0)); // Seed random number generator
+    int numberToGuess = rand() % 100 + 1; // Random number between 1 and 100
+    int userGuess;
+    int attempts = 0;
+
+    cout << "🎮 Welcome to the Number Guessing Game!" << endl;
+    cout << "I have picked a number between 1 and 100. Try to guess it!" << endl;
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> userGuess;
+        attempts++;
+
+        if (userGuess > numberToGuess) {
+            cout << "Too high! Try again.\n";
+        } else if (userGuess < numberToGuess) {
+            cout << "Too low! Try again.\n";
+        } else {
+            cout << " Congratulations! You guessed it right in " << attempts << " attempts." << endl;
+        }
+
+    } while (userGuess != numberToGuess);
+
+    return 0;
+}
